3dbeaa69ea5ee09c7a4b2b07c1463787b8f2d6f2
New-Tong <1248449622@qq.com>
2024-04-11 14:32:24 +0800

    SpringSSM之Mybatis学习

---
+++ part3-mybatis/mybatis-back-quick-01/src/test/java/com/shansong/mapper/EmployeeMapperTest.java
@@ -0,0 +1,39 @@
+package com.shansong.mapper;
+
+import com.shansong.pojo.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author ShanSong
+ * @Date 2024/4/11 -10:12
+ * 包名:多单词组成时所有字母都小写:xxxyyyzzz
+ * 类名、接口名:多单词组成时，所有单词的首字母大写:XxxYyyZzz
+ * 变量名、方法名:多单词组成时，第一个单词首字母小写，
+ * 第二个单词开始每个单词首字母大写: xxxYyyZzZ
+ * 常量名:所有字母都大写。多单词时每个单词用下划线连接:XXx_YW_zzz
+ * @Description
+ */
+public class EmployeeMapperTest {
+
+    @Test
+    public void testEmployeeMapper() {
+        String mybatisConfigFilePath = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(mybatisConfigFilePath);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee employee = employeeMapper.queryById(2);
+            System.out.println(employee);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
